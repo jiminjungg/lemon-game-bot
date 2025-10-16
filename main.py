15ce7cca90aaa79bd1b2f71061eea4735ee5cfd9
Jimin Jung <219709013+jiminjungg@users.noreply.github.com>
2025-10-16 15:37:48 -0700

    feat: implement ad skip for every ten games

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import argparse
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, sync_playwright
 
+AD_LOAD_TIMEOUT = 5000
+AD_SKIP_TIMEOUT = 32000
 LEMON_GAME_URL = "https://wwme.kr/lemon/play?mode=normal#goog_rewarded"
+
+
+def skip_ad(page: Page, is_first_skip: bool) -> None:
+    page.wait_for_timeout(AD_LOAD_TIMEOUT)
+    if is_first_skip:
+        ad_dialogue_child = page.get_by_text("광고 시청하기")
+        page.get_by_role("button").filter(has=ad_dialogue_child).click()
+    ad_view_child = page.get_by_text("광고 보기")
+    page.get_by_role("button").filter(has=ad_view_child).last.click()
+    page.wait_for_timeout(AD_SKIP_TIMEOUT)
+    ad_frame = page.get_by_title("3rd party ad content").last.content_frame
+    ad_frame.get_by_role("button", name="동영상 닫기").click()
+    page.get_by_text("확인").click()
 
 
 def run_lemon_game_bot(num_games: int) -> None:
@@ -12,6 +27,9 @@ def run_lemon_game_bot(num_games: int) -> None:
         )
         page = browser.new_page(no_viewport=True)
         page.goto(LEMON_GAME_URL)
+        for i in range(num_games):
+            if i % 10 == 0:
+                skip_ad(page, i == 0)
         browser.close()
 
 

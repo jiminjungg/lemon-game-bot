0b35310c47197557b22afb02bd2d0ce14e310e4f
Jimin Jung <219709013+jiminjungg@users.noreply.github.com>
2025-10-16 12:43:14 -0700

    feat: add argparse with --num_games argument

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import argparse
+
+
 def main() -> None:
-    print("Hello from lemon-game-bot!")
+    parser = argparse.ArgumentParser(
+        description="A script that plays Lemon Game through browser automation"
+    )
+    parser.add_argument(
+        "-n",
+        "--num_games",
+        default=1,
+        type=int,
+        help="number of times to play Lemon Game (default: %(default)s)",
+    )
+
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
